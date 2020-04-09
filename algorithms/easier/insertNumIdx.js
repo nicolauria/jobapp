2816213c6ba514a23e90e28fb0329bd8c75bb357
Nico Lauria <nflauria@outlook.com>
2020-04-08 20:06:22 -0700

    better insertNumIdx solution

--- algorithms/easier/insertNumIdx.js
+++ algorithms/easier/insertNumIdx.js
@@ -8,6 +8,11 @@ console.log(getIndex([10, 5, 1, 3], 2) === 1);
 console.log(getIndex([16, 4, 5], 13) === 1);
 
 /*
+const getIndex = (arr, number) =>
+    arr.reduce((counter, curr) => (number > curr ? ++counter : counter), 0);
+*/
+
+/*
 function getIndex(arr, number) {
   let sorted = arr.sort((a, b) => a - b);
   for (let i = 0; i < sorted.length; i++) {
